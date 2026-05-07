c063d0898d935249ea537bf292846bd2732558c4
Baygirafe4 <fortilove666@mail.ru>
2026-05-07 16:37:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.get("/auth/instagram", (req, res) => {
     "instagram_business_manage_messages"
   ].join(",");
 
-  const authUrl = `https://www.facebook.com/dialog/oauth?client_id=${APP_ID}&redirect_uri=${BASE_URL}/auth/callback&scope=instagram_basic,instagram_manage_messages,pages_show_list&response_type=code`;
+  const authUrl = `https://www.facebook.com/dialog/oauth?client_id=${APP_ID}&redirect_uri=${BASE_URL}/auth/callback&scope=instagram_business_basic,instagram_business_manage_messages&response_type=code`;
   res.redirect(authUrl);
 });
 
